4316228e02802970e0dc673bea0f9588fd2584ad
Muhammad Owais <rahneverd@gmail.com>
2024-04-03 10:54:21 +0300

    Auto-generated commit

--- src/environment/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  apiUrl: `https://api.openweathermap.org/data/2.5/weather`,
-  apiKey: 'b03dfa76b0b3ed707fe586b29c701f3c',
-};
-
-// ?q=${city}&appid=${API_KEY}&units=metric
-//
